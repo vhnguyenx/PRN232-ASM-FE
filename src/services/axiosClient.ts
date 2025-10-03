67012833ff3e7d58686fa13931c6eb19fd9f7391
Hoang Nguyen <vhnguyenx@gmail.com>
2025-10-03 15:47:58 +0700

    FIX

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: "https://prn232-asm-be.onrender.com/api",
   headers: {
     'Content-Type': 'application/json',
   },
